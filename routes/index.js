80f0c5b9c8b7c6532afcfff79f5ad06035250e1c
pramodtolani <pramodtolani91@gmail.com>
2020-01-06 23:17:49 +0530

    install nodemon and changes in index route for linux

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,15 @@ var router = express.Router();
 const os = require('os');
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-  const address = await os.networkInterfaces();
-	const mac = address['Wi-Fi'][0].mac;
-  // res.render('index', { title: 'Express' });
-  res.json({
-    mac: mac
-  });
+router.get('/', async function (req, res, next) {
+	const address = await os.networkInterfaces();
+
+	// const mac = address['Wi-Fi'][0].mac; //for windows
+	const mac = address['etho'][0].mac; //for linux
+	// res.render('index', { title: 'Express' });
+	res.json({
+		mac: mac
+	});
 });
 
 module.exports = router;
